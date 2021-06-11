99ba5720b47be272bbf1fb17fa52d37456c8bbd9
Yessoh bonny <63106083+YessohB@users.noreply.github.com>
2021-06-11 12:57:55 +0200

    Add files via upload

---
+++ parking2021/parking2021/parking2021/src/main/java/fr/miage/projet/parking2021/Parking2021Application.java
@@ -0,0 +1,13 @@
+package fr.miage.projet.parking2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Parking2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Parking2021Application.class, args);
+	}
+
+}
---
+++ parking2021/parking2021/parking2021/src/main/java/fr/miage/projet/parking2021/metier/Place.java
@@ -0,0 +1,7 @@
+package fr.miage.projet.parking2021.metier;
+
+public class Place {
+    private int numPlace;
+    private String typePlace ="";
+
+}
